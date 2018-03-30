81d70d927ea1bf1ef295c26fc8b956cc2c9176af
Seth Wax <sethlwax@gmail.com>
2018-03-30 19:26:04 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,6 +61,12 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 
+  holiday_hash.each do |season, holidays|
+    puts "#{season.capitalize}:"
+    holidays.each do |holiday, supply_array|
+      puts "  #{holiday.caplitalize}: " + supply_array.split(", ")
+    end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
