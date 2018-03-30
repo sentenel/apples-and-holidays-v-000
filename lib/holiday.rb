2ef72dfbc6a569d6dece331a671efb7d2c969cb4
Seth Wax <sethlwax@gmail.com>
2018-03-30 19:26:27 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays|
     puts "#{season.capitalize}:"
     holidays.each do |holiday, supply_array|
-      puts "  #{holiday.caplitalize}: " + supply_array.split(", ")
+      puts "  #{holiday.caplitalize}: " + supply_array.join(", ")
     end
   end
 end
